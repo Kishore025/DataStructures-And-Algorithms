a712fdbe2c206e5f140e18cfc04531d26f44db41
Kishore <simpro@Simpros-MacBook-Pro-3.local>
2022-12-09 23:53:43 +0530

    learned bubble sort but this is not that efficient

---
+++ 2.Codevolution-Javascript-Algorithms+Datastructures/JavascriptAlgorithms/Sorting-Algorithms/1-Bubble-sort/bubble-sort.js
@@ -0,0 +1,26 @@
+/***
+ * Bubble Sort
+ * Problem : Given an array of integers, sort the array
+ *
+ * const arr = [-6,20,8,-2,4]
+ * bubblesort(arr) => Should return [-6,-2,4,8,20]
+ */
+
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+
+console.log(bubbleSort([-6, 20, 8, -2, 4])); //
