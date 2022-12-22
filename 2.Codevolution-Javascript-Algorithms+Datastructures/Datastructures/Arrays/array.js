7be637d29a32f1be4b340221efe6956af9040faf
Kishore <simpro@Simpros-MacBook-Pro-3.local>
2022-12-22 14:25:51 +0530

    added time complexity for array methods

--- 2.Codevolution-Javascript-Algorithms+Datastructures/Datastructures/Arrays/array.js
+++ 2.Codevolution-Javascript-Algorithms+Datastructures/Datastructures/Arrays/array.js
@@ -1,3 +1,15 @@
+/***
+ * Array - Big-O time complexity
+ *
+ * Insert/remove from end - O(1)
+ * Insert/remove from beginning - O(n)
+ * Access - O(1)
+ * Search - O(n)
+ * push/pop - O(1)
+ * Shift/unshift/concat/slice/splice - O(n)
+ * forEach/map/filter/reduce - O(n)
+ */
+
 const arr = [1, 2, 3, "Kishore"];
 console.log(arr);
 
